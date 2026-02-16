32b8595e4ce94b33204ebd6742023f6874f87244
gernophil <gernophil@gmx.net>
2026-02-16 10:24:46 +0100

    removed dependency from ostendorflab package

--- R/flow_yo.R
+++ R/flow_yo.R
@@ -180,7 +180,22 @@ gate_fcs <- function(fcs_data,
     ggplot2::xlab(x) +
     ggplot2::ylab(y) +
     ggplot2::ggtitle(name) +
-    ostendorflab::theme_custom2()
+    ggplot2::theme(
+      # text = element_text(family = "Helvetica-Narrow"),
+      panel.grid = element_blank(),
+      axis.line = element_line(linewidth = custom_linewidth),
+      axis.ticks = element_line(linewidth = custom_linewidth),
+      axis.ticks.length = unit(0.075, "cm"),
+      # axis.text.x = element_text(hjust = 1),
+      axis.title = element_text(size = 6),
+      axis.text = element_text(size = 5, colour = "black"),
+      plot.title = element_text(size = 7, hjust = 0.5),
+      panel.background = element_blank(),
+      legend.text = element_text(size = 5),
+      legend.title = element_blank(),
+      # legend.margin = margin(t = -1, r = 0, l = -0.3, unit = 'cm'),
+      legend.key.size = unit(0.25, "line") # taken from ostendorflab::theme_custom2()
+    )
 
   if (plot_type == "hexagon") {
 
